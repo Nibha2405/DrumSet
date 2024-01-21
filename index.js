4b8997841418c22ea119d20646f9d4c05a8b7518
Nibha Kumari <98377632+Nibha2405@users.noreply.github.com>
2024-01-21 14:54:54 +0530

    Update index.js
    Animation added to the button on press.

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for ( var i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
         makeSound (buttonInnerHTML);
+        buttonAnimation (buttonInnerHTML);
     });
 }
 //Detecting keyboard press
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation (event.key);
 });
 function makeSound(key){
         switch (key) {
@@ -51,6 +53,13 @@ function makeSound(key){
             default: console.log(buttonInnerHtml);
     }
 }
+ function buttonAnimation (currentKey){
+    var activeButton = document.querySelector("." +currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+  }
        
   
 
